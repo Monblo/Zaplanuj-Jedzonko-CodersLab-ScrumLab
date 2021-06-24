a99a1d3d22dacfe5be94386ad732e985931b945f
slawekSk13 <slawomir.skrzeczynski@gmail.com>
2021-06-24 20:49:14 +0200

    poprawiona logika dodawania przepisów + walidacja formularza

--- development/js/app.js
+++ development/js/app.js
@@ -10,6 +10,7 @@ class ItemToHide {
         this.clickTarget.parentElement.querySelector('.app__widget--message--text').innerHTML = text;
 
     }
+
     addListener() { //after click parent of element is hidden
         this.clickTarget.addEventListener('click', (e) => e.target.parentElement.classList.add('hidden'));
     }
@@ -20,8 +21,6 @@ const widgetMessages = widgetMessagesList.map(el => new ItemToHide(el));
 widgetMessages.forEach(el => el.addListener());
 
 //Tworzenie przepisu
-const ingredients = [];
-const instructions = [];
 const form = {
     form: document.getElementById('recipe-form'),
     cancel: document.getElementById('cancel-recipe'),
@@ -36,53 +35,88 @@ const form = {
     ingredientsList: document.getElementById('ingredients-list'),
 }
 //Dodanie składnika lub instrukcji
-const addIn = [form.addInstruction, form.addIngredient];
-addIn.forEach(el => el.addEventListener('click', (e) => {
-        if (e.target === form.addInstruction) {
+const addToList = (e) => {
+    if (e.target === form.addInstruction) {
+        if(form.instruction.value.length > 50 || form.instruction.value.length < 5) {
+            showWarning('recipe-instruction-warning');
+        } else {
             const listElement = document.createElement('li');
             listElement.innerHTML = form.instruction.value + ' <i class="fas fa-edit"></i> <i class="fas fa-trash-alt"></i>';
             form.instructionsList.appendChild(listElement);
-            instructions.push(form.instruction.value);
-            form.instruction.value = 'Jaki jest następny krok?';
-        } else if (e.target === form.addIngredient) {
+            form.instruction.value = '';
+            form.instruction.setAttribute('placeholder', 'Jaki jest następny krok?');
+        }
+    } else if (e.target === form.addIngredient) {
+        if(form.ingredient.value.length > 150 || form.ingredient.value.length < 5) {
+            showWarning('recipe-ingredient-warning');
+        } else {
             const listElement = document.createElement('li');
             listElement.innerHTML = form.ingredient.value + ' <i class="fas fa-edit"></i> <i class="fas fa-trash-alt"></i>';
             form.ingredientsList.appendChild(listElement);
-            ingredients.push(form.ingredient.value);
-            form.ingredient.value = 'Jaki jest następny krok?';
-        } else {
-            console.log('something went wrong');
+            form.ingredient.value = '';
+            form.ingredient.setAttribute('placeholder', 'Jaki jest następny składnik?');
         }
+    } else {
+        console.log('something went wrong');
     }
-));
+}
+//Usuwanie ostrzeżeń dotyczących długości danych wprowadzonych w formularzu
+document.querySelectorAll('.warning').forEach(el => el.addEventListener('click', e => e.target.classList.add('hidden')));
+//Ukrywanie formularza
+const hideRecipeForm = () => form.form.classList.add('hidden');
+//Dodawanie elementów do list
+form.addInstruction.addEventListener('click', addToList);
+form.addIngredient.addEventListener('click', addToList);
 //Anulowanie wypełniania formularza
 form.cancel.addEventListener('click', () => {
     form.form.reset;
-    form.form.classList.add('hidden');
+    hideRecipeForm();
 });
 //Zapisanie przepisu
-form.save.addEventListener('click', (e) => {
+const showWarning = (id) => {
+    document.getElementById(id).classList.remove('hidden');
+}
+const save = (e) => {
     e.preventDefault();
-    let recipes = JSON.parse(localStorage.getItem('localRecipes'));
-    if(!recipes) {
-        recipes = [];
+    if (form.name.value.length > 50 || form.name.value.length < 2) {
+        showWarning('recipe-name-warning');
     }
-    const recipe = {
-        name: form.name.value,
-        description: form.description.value,
-        instructions: instructions,
-        ingredients: ingredients
+    if (form.description.value.length > 360 || form.description.value.length < 10) {
+        showWarning('recipe-description-warning');
+    } else {
+        let recipes = JSON.parse(localStorage.getItem('localRecipes'));
+        if (!recipes) {
+            recipes = [];
+        }
+        const instructions = document.querySelectorAll('#instructions-list li');
+        const inst = [];
+        instructions.forEach(el => inst.push(el.innerText.slice(0, -2)));
+        const ingredients = document.querySelectorAll('#ingredients-list li');
+        const ingr = [];
+        ingredients.forEach(el => ingr.push(el.innerText.slice(0, -2)));
+        const recipe = {
+            name: form.name.value,
+            description: form.description.value,
+            instructions: inst,
+            ingredients: ingr
+        }
+
+        recipes.push(recipe);
+        localStorage.setItem('localRecipes', JSON.stringify(recipes));
+        console.log(JSON.parse(localStorage.getItem('localRecipes')));
+        hideRecipeForm();
+        counterUpdate();
     }
-    recipes.push(recipe);
-    localStorage.setItem('localRecipes', JSON.stringify(recipes));
-    console.log(JSON.parse(localStorage.getItem('localRecipes')));
-   form.form.classList.add('hidden');
-    counterUpdate();
-});
+}
+//Uaktualnienie licznika przepisów
 const counterUpdate = () => {
     const l = JSON.parse(localStorage.getItem('localRecipes'));
     document.getElementById('recipes-lenght').innerText = l.length;
 }
-window.addEventListener('DOMContentLoaded',() => counterUpdate());
+form.save.addEventListener('click', save);
+
+window.addEventListener('DOMContentLoaded', () => counterUpdate());
 
 document.getElementById('widget-add-recipe').addEventListener('click', () => form.form.classList.remove('hidden'));
+
+const check = document.querySelectorAll('#instructions-list li');
